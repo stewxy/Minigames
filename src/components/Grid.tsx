f3c7dc6a9e250b1902c300b6d4a302dc0f4d5185
Stephen <stewxy13@gmail.com>
2023-09-19 04:43:32 +1200

    added css to "grid" component of tictactoe

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -2,10 +2,16 @@ import React from "react";
 import "../App.css";
 
 const Grid = () => {
+  function handleClick() {
+    console.log("Clicked");
+  }
+
   return (
     <div className="container text-center">
       <div className="row row-cols-3 ">
-        <div className="col">Column</div>
+        <div className="col" onClick={handleClick}>
+          Column
+        </div>
         <div className="col">Column</div>
         <div className="col">Column</div>
       </div>
