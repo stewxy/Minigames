c84c8b0ea4af47ddd0529b931ca7b9baddac6356
Stephen <stewxy13@gmail.com>
2023-09-20 06:18:44 +1200

    implemented state for grid component to change text when clicked (incomplete)

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -1,16 +1,27 @@
 import React from "react";
 import "../App.css";
+import { useState } from "react";
 
 const Grid = () => {
+  let test = 1;
+  const [value, setValue] = useState("");
+
   function handleClick() {
     console.log("Clicked");
+    //setValue("X");
+    test += 1;
+    if (test % 2 != 0) {
+      setValue("X");
+    } else {
+      setValue("O");
+    }
   }
 
   return (
     <div className="container text-center">
       <div className="row row-cols-3 ">
         <div className="col" onClick={handleClick}>
-          Column
+          {value}
         </div>
         <div className="col">Column</div>
         <div className="col">Column</div>
