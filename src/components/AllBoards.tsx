a2fee274cff67e347251d39514cd41909669b7ca
Stephen <stewxy13@gmail.com>
2023-10-04 17:45:54 +1300

    added clicking funtionality to boards

--- src/components/AllBoards.tsx
+++ src/components/AllBoards.tsx
@@ -1,63 +1,69 @@
 import React, { useState } from "react";
 import "../App.css";
 
-const [click, setClicked] = useState(false);
-
 interface Props {
   value: String;
 }
 
-function Square({ value }: Props) {
-  return <button className="Grid">{value}</button>;
-}
-
-function SingleBoard() {
-  return (
-    <div className="oneBoard">
-      <div className="squareRow">
-        <Square value={"1"} />
-        <Square value={"2"} />
-        <Square value={"3"} />
-      </div>
-      <div className="squareRow">
-        <Square value={"4"} />
-        <Square value={"5"} />
-        <Square value={"6"} />
-      </div>
-      <div className="squareRow">
-        <Square value={"7"} />
-        <Square value={"8"} />
-        <Square value={"9"} />
-      </div>
-    </div>
-  );
-}
-
-function UltimateSquare() {
-  return (
-    <div className="fullBoard">
-      <div className="ultimateRow">
-        <SingleBoard />
-        <SingleBoard />
-        <SingleBoard />
-      </div>
-      <div className="ultimateRow">
-        <SingleBoard />
-        <SingleBoard />
-        <SingleBoard />
-      </div>
-      <div className="ultimateRow">
-        <SingleBoard />
-        <SingleBoard />
-        <SingleBoard />
-      </div>
-    </div>
-  );
-}
-
-function handleClick() {}
-
 const AllBoards = () => {
+  //const [click, setClicked] = useState(false);
+  const [value, setValue] = useState("");
+
+  function Square() {
+    return (
+      <button className="Grid" onClick={handleClick}>
+        {value}
+      </button>
+    );
+  }
+
+  function SingleBoard() {
+    return (
+      <div className="oneBoard">
+        <div className="squareRow">
+          <Square />
+          <Square />
+          <Square />
+        </div>
+        <div className="squareRow">
+          <Square />
+          <Square />
+          <Square />
+        </div>
+        <div className="squareRow">
+          <Square />
+          <Square />
+          <Square />
+        </div>
+      </div>
+    );
+  }
+
+  function UltimateSquare() {
+    return (
+      <div className="fullBoard">
+        <div className="ultimateRow">
+          <SingleBoard />
+          <SingleBoard />
+          <SingleBoard />
+        </div>
+        <div className="ultimateRow">
+          <SingleBoard />
+          <SingleBoard />
+          <SingleBoard />
+        </div>
+        <div className="ultimateRow">
+          <SingleBoard />
+          <SingleBoard />
+          <SingleBoard />
+        </div>
+      </div>
+    );
+  }
+
+  function handleClick() {
+    setValue("X");
+  }
   return UltimateSquare();
 };
 
