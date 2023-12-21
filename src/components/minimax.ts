7a7db505923147171c13ae4d8de2c6ef96777045
Stephen <stewxy13@gmail.com>
2023-12-22 03:22:36 +1300

    implementing minimax algorithm, progress

--- src/components/minimax.ts
+++ src/components/minimax.ts
@@ -1,27 +1,58 @@
 import React from 'react'
 
+//PLAYER WANTS TO CHOOSE MAXIMISING SCORE (+10)
+//AI WANTS TO CHOOSE MINIMISING SCORE (-10)
+//0 FOR DRAW
+
 let scores = {
     X: 1,
     O: -1,
     tie: 0
 };
-export function minimax(squares:any, depth:number) {
+
+
+
+export function minimax(squares:any, depth:number, isMaximising: boolean){
     let bestScore = Number.NEGATIVE_INFINITY;
     let bestMove;
-    //return ("# " + value + " #");
     for(let i=0; i<9; i++){
-        console.log("")
         if(squares[i] == null){
-            console.log();
-            let score = minimax(squares, depth+=1);
-            // if(score > bestScore){
-            //     bestScore = score;
-            //     bestMove = i;
-            // }
-            bestScore = Math.max(score, bestScore);
-            
+            squares[i] = "O"
+            let score = algorithm(squares, 0, true);
+            squares[i] = null;
+            if(score > bestScore){
+                bestScore = score;
+                bestMove = i;
+            }
         }
     }
-    console.log(bestScore);
-    return bestScore;
+    return bestMove;
+}
+
+function algorithm(squares:any, depth:number, isMaximising: boolean) {
+    //PLAYER (X, MAXIMISER)
+    if (isMaximising){
+        let bestScore = Number.NEGATIVE_INFINITY;
+        for(let i=0; i<9; i++){
+            if(squares[i] == null){
+                squares[i] = "X"
+                let score = algorithm(squares, depth+=1, false);
+                bestScore = Math.max(score, bestScore);
+                squares[i] = null;
+            }
+        }
+        return bestScore;
+    //AI (O, MINIMISER)
+    } else {
+        let bestScore = Number.POSITIVE_INFINITY;
+        for(let i=0; i<9; i++){
+            if(squares[i] == null){
+                squares[i] = "O"
+                let score = algorithm(squares, depth+=1, true);
+                bestScore = Math.min(score, bestScore);
+                squares[i] = null;
+            }
+        }
+        return bestScore;
+    }
 }
